cfd59148da198d665110f20f36f3d23423e735c1
Mathias Dolag <mathias.dolag@isys.de>
2018-01-24 21:19:20 +0100

    Add Javadoc

--- src/main/java/de/duckdeer/expenses/repository/ExpenseRepository.java
+++ src/main/java/de/duckdeer/expenses/repository/ExpenseRepository.java
@@ -9,7 +9,23 @@ import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
+    /**
+     * Find {@link Expense}s for certain {@link Category}
+     *
+     * @param categoryId
+     *  id of specific {@link Category}
+     * @return
+     *  {@link Expense}s with {@link Category} identified by given id
+     */
     List<Expense> listByCategory(@Param("catId") Long categoryId);
 
+    /**
+     * Find {@link Expense}s for certain {@link Category}s
+     *
+     * @param categoryIds
+     *  ids of specific {@link Category}s
+     * @return
+     *  {@link Expense}s with {@link Category} identified by given id list
+     */
     List<Expense> listByCategories(@Param("catIds") List<Long> categoryIds);
 }
